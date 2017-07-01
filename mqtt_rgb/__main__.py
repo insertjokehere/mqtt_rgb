cce02a1986ed296e5d1f327d149039ceae267771
William Hughes <whlabratz@gmail.com>
2017-07-01 23:06:06 +1200

    Initial commit

---
+++ mqtt_rgb/__main__.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+import unicornhat as unicorn
+
+
+class App():
+
+    def _init_unicorn(self):
+        unicorn.set_layout(unicorn.AUTO)
+        unicorn.rotation(0)
+        unicorn.brightness(0.5)
+        time.sleep(1)
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe("unicorn/#")
+
+    def on_message(self, client, userdata, msg):
+
+        _, x, y = msg.topic.split("/")
+        colour = json.loads(msg.payload)
+
+        unicorn.set_pixel(int(x), int(y),
+                          int(colour['r']), int(colour['g']), int(colour['b']))
+        unicorn.show()
+
+    def _connect(self):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.client.connect("localhost", 1883, 60)
+
+    def _run(self):
+        self.client.loop_forever()
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='mqtt_rgb',
+    version='0.0.1',
+    description='MQTT-to-Unicorn bridge',
+    long_description=readme,
+    author='Will Hughes',
+    url='https://github.com/insertjokehere/mqtt_rgb',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
