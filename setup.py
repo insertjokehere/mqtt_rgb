88adae09eece00ff01fbfd5d9aa1eb554bd19fcb
William Hughes <whlabratz@gmail.com>
2017-07-01 23:29:07 +1200

    Set install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from setuptools import setup, find_packages
 
-
 with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
+
+with open('requirements.txt') as f:
+    install_reqs = f.read()
+
 
 setup(
     name='mqtt_rgb',
@@ -17,5 +20,6 @@ setup(
     author='Will Hughes',
     url='https://github.com/insertjokehere/mqtt_rgb',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=install_reqs
 )
